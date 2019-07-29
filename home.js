ae2cacbd2e060fca51544fed87e8a54eab05c594
Gagan Sharma <43646043+codeongags@users.noreply.github.com>
2019-07-29 14:23:58 +0530

    Initial commit

---
+++ home.js
@@ -0,0 +1,29 @@
+function search(){
+    document.getElementById('searchbar').style.width="200px";
+    document.getElementById('searchbar').style.border="3px solid black";
+    document.getElementById('butse').innerHTML="&#128269;";
+    document.getElementById('sercol').style.display="inline-block";
+}
+function searchcol(){
+    document.getElementById('searchbar').style.width="0px";
+    document.getElementById('searchbar').style.border="0px solid black";
+    document.getElementById('butse').innerHTML="&#128269; Search";
+    document.getElementById('sercol').style.display="none";
+}
+function closemenu()
+{document.getElementById('sidebar').style.width="0px";
+document.getElementById('sidebar').style.display="none";
+document.getElementById('close').style.display="none";
+document.getElementById('menu').style.display="block"
+
+
+}
+ function openmenu()
+ {document.getElementById('sidebar').style.display="block";
+ document.getElementById('menu').style.display="none";
+ document.getElementById('sidebar').style.width="20%"
+ document.getElementById('close').style.display="block"
+ document.getElementById('content').style.width="80%"
+
+
+}
